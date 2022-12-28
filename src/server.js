8370e9e5f1529fe9759dfab6947bbbbd16fe6c55
Donna Phan <102575544+Dphan97@users.noreply.github.com>
2022-12-28 02:30:09 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,18 @@
-const { PORT = 5001 } = process.env;
+const { PORT = 5432 } = process.env;
+
+const app = require("./app");
+const knex = require("./db/connection");
+
+const listener = () => console.log(`Listening on Port ${PORT}!`);
+
+knex.migrate
+  .latest()
+  .then((migrations) => {
+    console.log("migrations", migrations);
+    app.listen(PORT, listener);
+  })
+  .catch(console.error);
+1 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
