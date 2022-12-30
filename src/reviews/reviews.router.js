61c5a9e65e432bff400893f20effb60e6317da8f
Donna Phan <102575544+Dphan97@users.noreply.github.com>
2022-12-29 22:26:31 -0800

    Update reviews.router.js

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -1,11 +1,35 @@
-const router = require("express").Router();
-const controller = require("./reviews.controller");
-const methodNotAllowed = require("../errors/methodNotAllowed");
+const knex = require("../db/connection"); 
+const mapProperties = require("../utils/map-properties");
 
-router
-    .route("/:reviewId")
-    .put(controller.update)
-    .delete(controller.delete)
-    .all(methodNotAllowed);
+async function read(reviewId) {
+    return knex("reviews as r")
+        .join("critics as c", "c.critic_id", "r.critic_id")
+        .select("r.*", "c.*")
+        .where({"review_id":reviewId})
+        .first()
+        .then(addCritic);
+}
 
-module.exports = router;
+const addCritic = mapProperties({
+    preferred_name: "critic.preferred_name",
+    surname: "critic.surname",
+    organization_name: "critic.organization_name",
+  });
+
+async function update(updatedReview) {
+    return knex("reviews")
+        .select("*")
+        .where({ review_id: updatedReview.review_id })
+        .update(updatedReview, "*");
+}
+
+async function destroy(reviewId) {
+    return knex("reviews")
+        .where({"review_id":reviewId}).del();
+}
+
+module.exports = {
+    read,
+    update,
+    delete: destroy,
+}
